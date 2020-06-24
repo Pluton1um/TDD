ea22685609427ea6fa44946499ec96aa9399a6e1
caseyhan/X2017522041 <1124011866@qq.com>
2020-06-24 23:50:53 +0800

    first steps of FT for layout + styling

--- lists/views.py
+++ lists/views.py
@@ -1,29 +1,24 @@
 from django.shortcuts import render, redirect
-from lists.models import Item
+from lists.models import Item, List
 from django.http import HttpResponse
 
 
 # Create your views here.
 def home_page(request):
     # if request.method == 'POST':
-    #     return HttpResponse(request.POST['item_text'])
-    # item = Item()
-    # item.text = request.POST.get('item_text', '')
-    # item.save()
-
-    if request.method == 'POST':
-        # new_item_text = request.POST['item_text']
-        # Item.objects.create(text=new_item_text)
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
-    # else:
-    #     new_item_text = ''
-
-    items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
-    # return render(request, 'home.html')
-
+    #     Item.objects.create(text=request.POST['item_text'])
+    #     return redirect('/lists/the-only-list-in-the-world/')
+    return render(request, 'home.html')
     # return render(request, 'home.html', {
     #     # 'new_item_text': request.POST.get('item_text', ''),
     #     'new_item_text': new_item_text,
     # })
+
+def view_list(request):
+    items = Item.objects.all()
+    return render(request, 'list.html', {'items': items})
+
+def new_list(request):
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect('/lists/the-only-list-in-the-world/')
